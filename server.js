8c3de28311e0a5d4b5ce2a89e5ec9f5c29906aa4
siddrajpoot@gmail.com <siddrajpoot@gmail.com>
2018-02-27 19:55:08 -0800

    removed server file

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ const path = require('path');
 const serveStatic = require('serve-static');
 
 const app = express();
-app.use(serveStatic(path.join(__dirname, '../client/dist')));
-
-// app.get('/', (req, res) => {
-//   res.send({message: 'Hello world'});
-// });
+app.use(serveStatic(path.join(__dirname, './client/dist')));
 
 const port = process.env.PORT || 3001;
 
